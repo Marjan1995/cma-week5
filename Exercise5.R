99d6b16cfbedab68105477babb08f95789711e31
Marjan Tabataba <marjan.tabatabavakili@uzh.ch>
2021-05-28 10:31:46 +0200

    task 1 finish

--- Exercise5.R
+++ Exercise5.R
@@ -8,3 +8,13 @@ library(tmap)
 
 wildschwein_BE <- read_delim("wildschwein_BE_2056.csv",",") %>%
   st_as_sf(coords = c("E", "N"), crs = 2056, remove = FALSE)
+
+# Task 1
+fields <- read_sf("Feldaufnahmen_Fanel.gpkg")
+ggplot(fields, aes(fill = Frucht)) +
+geom_sf() +
+coord_sf(datum = 2056)
+fields
+
+
+# Task 2
