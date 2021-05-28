6abfe9b772f2f6923b36c5529947f5bb67230859
Marjan Tabataba <marjan.tabatabavakili@uzh.ch>
2021-05-28 10:27:06 +0200

    preparation

---
+++ Exercise5.R
@@ -0,0 +1,10 @@
+library(readr)
+library(sf)
+library(terra)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(tmap)
+
+wildschwein_BE <- read_delim("wildschwein_BE_2056.csv",",") %>%
+  st_as_sf(coords = c("E", "N"), crs = 2056, remove = FALSE)
